778f66f24c186845a57452498b89dd7901677102
IcaroLW <icarowanzeler16@gmail.com>
2024-01-04 22:22:00 -0300

    primeiro commit

---
+++ app.vue
@@ -0,0 +1,34 @@
+
+<head>
+
+</head>
+
+
+<template>
+  <header class=" bg-[url('/img/programming.jpg')] bg-center h-[70px]">   
+    <div class=" bg-black h-[70px] w-[screen] opacity-60"> </div> 
+    <div class=" text-white absolute top-3 left-10 text-4xl"> ICARO </div>
+  
+  <div class=" text-white inline-flex m-[22px] absolute right-0 top-0">
+  
+  <div class="mr-14">Sobre</div>
+  <div class="mr-14">Meus Projetos</div>
+  <div class="mr-14">Tecnologias</div>
+
+  </div>
+
+  </header>
+  
+  
+  <div class="bg-white h-[600px] w-[screen]"> 
+    <div class=" ">
+
+    </div>
+
+  </div>
+ 
+ 
+ <div class="bg-green-600 h-[500px] w-[screen]"> </div>
+</template>
+
+
