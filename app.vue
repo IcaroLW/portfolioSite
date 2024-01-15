61caedd6b1e074611b3a728bc400719905757cb8
IcaroLW <icarowanzeler16@gmail.com>
2024-01-14 22:37:54 -0300

    aba de tecnologias add

--- app.vue
+++ app.vue
@@ -1,39 +1,57 @@
 <template>
 
-<div class="bg-background h-[700px] w-[screen]">  
-  <div class=" bg-white h-[70px] w-[screen] dark:shadow-2xl"> </div> 
-  <a href="a" class=" text-black absolute top-3 left-10 text-4xl"> WANZELER </a>
+<div class="bg-background h-[760px] w-[screen]">  
+  <div class=" bg-slate-950 h-[70px] w-[screen] dark:shadow-2xl"> </div> 
+  <a href="a" class=" text-white absolute top-3 left-10 text-4xl"> WANZELER </a>
 
   
-<div class=" text-black font-medium text-lg inline-flex ml-[22px] mt-[20px] absolute right-0 top-0"> 
+<div class=" text-white text-lg inline-flex ml-[22px] mt-[20px] absolute right-0 top-0"> 
   
-<a href="" class="mr-14 hover:text-blue-800 duration-300">SOBRE </a>
-<a href="" class="mr-14 hover:text-blue-800 duration-300">MEUS PROJETOS </a>
-<a href="" class="mr-14 hover:text-blue-800 duration-300">TECNOLOGIAS </a>
+<a href="" class="mr-14 hover:text-blue-600 duration-300">SOBRE </a>
+<a href="" class="mr-14 hover:text-blue-600 duration-300">MEUS PROJETOS </a>
+<a href="" class="mr-14 hover:text-blue-600 duration-300">TECNOLOGIAS </a>
 
 </div>
 
   
-<div class="bg-black h-[350px] w-[screen] mt-[135px] opacity-60 hover:opacity-90 duration:700 hover:rounded-2xl duration-500 ">
+<div class="bg-black h-[400px] w-[screen] mt-[150px] opacity-60 hover:opacity-90 duration:700 hover:rounded-2xl duration-500 ">
 </div>
-<div class=" text-white mt-[-280px] ml-[460px] tracking-wider font-medium absolute text-3xl"> Olá, meu nome é Ícaro... <br> <a class="text-green-600"> Full-Stack </a> Developer <br> <br> <a class=" text-sm font-extralight"> Conheça um pouco mais sobre meu trabalho abaixo: </a>  </div>
-<div class=" text-white mt-[-200px] ml-[950px] absolute"> "Never regret thy fall, O Icarus of the fearless flight, <br> For the greatest tragedy of them all, Is never to feel the burning light." <br> </div>
+<div class=" text-white mt-[-300px] ml-[460px] tracking-wider font-medium absolute text-3xl"> Olá, meu nome é Ícaro... <br> <a class="text-green-600"> Full-Stack </a> Developer <br> <br> <a class=" text-sm font-extralight"> Conheça um pouco mais sobre meu trabalho abaixo: </a>  </div>
+<div class=" text-white mt-[-230px] ml-[950px] absolute"> "Never regret thy fall, O Icarus of the fearless flight, <br> For the greatest tragedy of them all, Is never to feel the burning light." <br> </div>
 <a href="https://github.com/IcaroLW" button type="button"  class=" text-white bg-slate-700 h-[35px] w-[200px] rounded-lg mt-[-120px] ml-[450px] absolute hover:bg-slate-800 duration-500 "> <div class=" bg-github bg-cover h-[40px] w-[40px] mb-[10px] mt-[-2px] ml-[3px] absolute "> </div> <div class=" ml-[48px] font-medium mt-[5px] absolute"> Acesse meu GitHub </div> </a>
 <a href="https://www.linkedin.com/in/%C3%ADcaro-wanzeler-791a46234/" button type="button" class=" text-white bg-slate-700 h-[35px] w-[200px] rounded-lg mt-[-120px] ml-[660px] absolute hover:bg-slate-800 duration-500"> <div class=" bg-linkedin bg-cover h-[38px] w-[38px] mt-[-1.5px] ml-[3px] absolute "> </div> <div class="ml-[40px] font-medium mt-[5px] absolute"> Acesse meu Linkedin </div> </a>
 
-<div class="bg-perfil bg-cover opacity-80 h-[300px] w-[300px] top-[232px] ml-[80px] absolute border-[3px] border-purple-900 rounded-r-full"> </div>
-
+<div class="bg-perfil bg-cover opacity-80 h-[300px] w-[300px] top-[270px] ml-[80px] absolute border-[3px] border-purple-900 rounded-r-full"> </div>
 
 </div>
 
 
+<div class=" bg-gradient-to-r from-blue-900 to-purple-700 h-[250px] w-[screen]">
+  <a class=" ml-[42%] mt-[1%] text-5xl font-medium absolute align-middle"> Tecnologias: </a>
+
+  <div class=" ml-[16%] inline-flex mt-[7%]"> 
+  
+  <div class="bg-python bg-cover ml-[40px] w-[100px] h-[100px] border-[3px] rounded-lg"></div> 
+
+  <div class="bg-tailwind bg-cover ml-[40px] w-[100px] h-[100px] border-[3px] rounded-lg"></div>
+
+  <div class="bg-nuxt bg-cover ml-[40px] w-[100px] h-[100px] border-[3px] rounded-lg"></div>
+  <div class="bg-javascript bg-cover ml-[40px] w-[100px] h-[100px] border-[3px] rounded-lg"></div>
+  <div class="bg-vuejs bg-cover ml-[40px] w-[100px] h-[100px] border-[3px] rounded-lg"></div>
+  <div class="bg-css bg-cover ml-[40px] w-[100px] h-[100px] border-[3px] rounded-lg"></div>
+  <div class="bg-html bg-cover ml-[40px] w-[100px] h-[100px] border-[3px] rounded-lg"></div>
+
+
+  </div>
+
+ 
+</div>
   
 
+<div class="bg-green-600 h-[600px] w-[screen]">
+
  
- 
- 
- <div class="bg-green-600 h-[500px] w-[screen]"> </div>
+</div>
+
+
 </template>
-
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -14,7 +16,14 @@ export default {
         'perfil': "url('/img/perfil.png')",
         'background': "url('/img/Background.png')",
         'github': "url('/img/github.svg')",
-        'linkedin': "url('/img/linkedin.svg')"
+        'linkedin': "url('/img/linkedin.svg')",
+        'vuejs': "url('/img/vue.svg')",
+        'python': "url('/img/python.svg')",
+        'javascript': "url('/img/javascript.svg')",
+        'tailwind': "url('/img/tailwind.svg')",
+        'html': "url('/img/html.svg')",
+        'css': "url('/img/css.svg')",
+        'nuxt': "url('/img/nuxt.svg')"
       }
     },
   },
